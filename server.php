9c4b8df1d31b258cb7e5a1309cef5008b1e0a955
root <root@ip-172-31-16-152.us-east-2.compute.internal>
2019-09-07 05:37:55 +0000

    Fixed Partner Not Inserting to DB Issue

--- server.php
+++ server.php
@@ -8,10 +8,11 @@
 	$workcomp= '';
 	$errors = array(); 
 	$_SESSION['success'] = "";
-
 	// connect to database
-	$db = mysqli_connect('localhost', 'root', '', 'doorregister');
-
+	$db = mysqli_connect('localhost', 'door2', 'door2', 'doorregister');
+        if (!$db) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
 	// REGISTER USER
 	if (isset($_POST['reg_user'])) {
 		function array_implode(array $workind)
@@ -25,7 +26,10 @@ $workind  = array_implode($_POST['workind']);
 		$middlename = mysqli_real_escape_string($db, $_POST['middlename']);
 		$dob1 = mysqli_real_escape_string($db, $_POST['dob']);
 		$dob = date('Y-m-d', strtotime(str_replace('/', '-', $dob1)));
-		$gender = mysqli_real_escape_string($db, $_POST['gender']);
+		//$today = date("Y-m-d");
+                //$age = date_diff(date_create($dob1), date_create($today));
+                $age = 25;
+                $gender = mysqli_real_escape_string($db, $_POST['gender']);
 		$expyrs = mysqli_real_escape_string($db, $_POST['expyrs']);
 		$mobile1 = mysqli_real_escape_string($db, $_POST['mobile1']);
 		$emailid = mysqli_real_escape_string($db, $_POST['emailid']);
@@ -37,15 +41,18 @@ $workind  = array_implode($_POST['workind']);
 		if (empty($mobile1)) { array_push($errors, "Mobile Number is required"); }
 		
 		
-
 		// register user if there are no errors in the form
 		if (count($errors) == 0) {
-			$query = "INSERT INTO users (id,firstname, lastname, middlename,fathername,dob,age,gender,nationality,Marital,profiles,
+			$query = "INSERT INTO users (firstname, lastname, middlename,fathername,dob,age,gender,nationality,Marital,profiles,
 			workind,workindmore,workcomp,workcompmore,pastexp,expyrs,localadd,permadd,mobile1,mobile2,emailid,Languages,idproof,addproof,panno,familyref,friendref,profilepic,idupload,addupload) 
-					  VALUES(' ','$firstname', '$lastname', '$middlename','', '$dob', '','$gender', '', '','',
+					  VALUES('$firstname', '$lastname', '$middlename','', '$dob', '$age','$gender', '', '','',
 					   '$workind', '','', '', '','$expyrs','', '','$mobile1', '','$emailid', '', '','', '', '', '', '', '', '')";
-			mysqli_query($db, $query);
+			if(mysqli_query($db, $query)){
+                            echo "New record created successfully";
+                        } else {
+                            echo "Error: " . $sql . "<br>" . mysqli_error($db);
+                        }
+                        mysqli_close($db);
 		}
-
 	}
 ?>
